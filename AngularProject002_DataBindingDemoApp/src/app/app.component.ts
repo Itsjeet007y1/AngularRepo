08f6ea9a6e5c70e3117eaea971a88be4f27b0d0f
admin <admin@192.168.0.2>
2018-09-17 12:55:47 +0530

    Refresh component file as new one

--- AngularProject002_DataBindingDemoApp/src/app/app.component.ts
+++ AngularProject002_DataBindingDemoApp/src/app/app.component.ts
@@ -8,17 +8,4 @@ import { Employee, EmployeeType } from './Employee';
 })
 export class AppComponent {
   title = 'Angular 4 Application !!';
-  faculty = 'Jitendra Kumar';
-  duration = 300;
-  facultyAvailability = true;
-  relatedCourses = ['Core Java', 'Advance Java', 'Spring', 'Hibernate,', 'Jpa', 'Angular'];
-  supportingEmployees: Employee[] = [
-    new Employee(1, 'Santosh', 500000, EmployeeType.Permanet),
-    new Employee(2, 'Ashok', 400000, EmployeeType.Permanet),
-    new Employee(3, 'Mahendra', 300000, EmployeeType.Temporary),
-    new Employee(4, 'Jitendra', 200000, EmployeeType.Temporary),
-    new Employee(5, 'Dharmendra', 100000, EmployeeType.Contract),
-    new Employee(6, 'Rajendra', 100000, EmployeeType.Contract)
-  ];
-  selectedSupportingEmployee: Employee = this.supportingEmployees[3];
 }
