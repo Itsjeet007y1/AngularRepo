6a5f496d002cd7da867321405a7cd01690756048
admin <admin@192.168.0.2>
2018-09-17 17:58:54 +0530

    changed for two way binding

--- AngularProject002_DataBindingDemoApp/src/app/app.component.ts
+++ AngularProject002_DataBindingDemoApp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Employee, EmployeeType } from './Employee';
+import { Person } from './Person';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,36 @@ export class AppComponent {
   developerName = 'Jitendra Kumar';
   isModified = true;
   evilTitle = '<script>alert("evil never sleeps")</script>';
+  eventType = 'NoEvent';
+  keys = '';
+  value = '';
   onClick() {
-    alert('Button Clicked');
+    const input = [1, 3];
+    const [first, second] = input;
+    alert('Button Clicked' + first + ' ' + second);
+  }
+  /*onMouseHover(evt: any) {
+    evt.target.src = '/assets/images/smiley1.png';
+    this.eventType = evt.type;
+  }
+  onMouseOut(evt: any) {
+    evt.target.src = '/assets/images/smiley2.png';
+    this.eventType = evt.type;
+  }*/
+  onMouse(evt: any) {
+    if (evt.type === 'mouseover') {
+      evt.target.src = '/assets/images/smiley1.png';
+      this.eventType = evt.type;
+    } else {
+      evt.target.src = '/assets/images/smiley2.png';
+      this.eventType = evt.type;
+    }
+  }
+  onKey(evt: any) {
+    this.keys += evt.key;
+    this.value = evt.target.value;
+  }
+  onKey1(value: string) {
+    this.value = value;
   }
 }
