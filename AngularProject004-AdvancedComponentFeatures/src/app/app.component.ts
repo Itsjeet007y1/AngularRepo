f4c29803d062a452173fa69da9be4e30e8f6c3c0
admin <admin@192.168.0.2>
2018-09-18 17:20:23 +0530

    Life Cycle of component hook

--- AngularProject004-AdvancedComponentFeatures/src/app/app.component.ts
+++ AngularProject004-AdvancedComponentFeatures/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, OnDestroy, DoCheck, AfterContentInit,
+  AfterContentChecked, AfterViewInit, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+@Component({
+  selector: 'app-success',
+  template: `<p>This is success...</p>`
+})
+  @Component({
+  selector: 'app-failure',
+  template: `<p>This is failure...</p>`
+})
+export class AppComponent implements OnInit, OnChanges, OnDestroy, DoCheck, AfterContentInit,
+  AfterContentChecked, AfterViewInit, AfterViewChecked {
   title = 'Angular 4 Application !!';
+  ngOnInit() {
+    console.log('ngOnInit fired');
+  }
+  ngOnChanges() {
+    console.log('ngOnChanges fired');
+  }
+  ngOnDestroy() {
+    console.log('ngOnDestroy fired');
+  }
+  ngDoCheck() {
+    console.log('ngDoCheck fired');
+  }
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit fired');
+  }
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked fired');
+  }
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit fired');
+  }
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked fired');
+  }
 }
--- AngularProject004-AdvancedComponentFeatures/src/app/app.module.ts
+++ AngularProject004-AdvancedComponentFeatures/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -10,9 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
