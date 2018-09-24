3cc5b6b4ddcb2ac4c4074ba8f2917264c6493341
admin <admin@192.168.0.2>
2018-09-24 17:22:12 +0530

    Created Http Service angular project

--- AngularProject011-AngularHttpService/src/app/app.component.ts
+++ AngularProject011-AngularHttpService/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular 4 Application !!';
+  title = 'Angular Tutorial Project !!';
+  todaydate;
+  jsonvalue = {name: 'JitendraKumar', desg: 'softwareEngineer', salary: 'confidential'};
+  constructor(private http: Http) {}
+  httpdata1;
+  ngOnInit() {
+    this.http.get("http://localhost:2020/AMHI/rest/getData").
+      map((response) => response.json()).
+      subscribe((data) =>{this.displaydata(data);})
+  }
+  displaydata(data) {this.httpdata1 = data;}
 }
